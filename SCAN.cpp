38f35edf8c20e6c07e950d5561821921bdbfa039
YovelaZ <145651975+YovelaZ@users.noreply.github.com>
2025-10-17 14:48:33 +0800

    Update SCAN.cpp

--- SCAN.cpp
+++ SCAN.cpp
@@ -29,22 +29,7 @@ void SCAN(statistics& stat){
                 stat.out_tensor_2[x_index][y_index] = density;
             }     
 		}
-    } else if (stat.dim==3){
-        for (int x_index = 0; x_index < stat.n_x; x_index++){
-			stat.q[0] = stat.x_L + x_index * stat.incr_x;
-            for(int y_index = 0; y_index < stat.n_y; y_index++){
-                stat.q[1] = stat.y_L + y_index * stat.incr_y;
-                for(int z_index = 0; z_index < stat.n_z; z_index++){
-                    stat.q[2] = stat.z_L + z_index * stat.incr_z;
-                    double density = 0;
-                    for (int i = 0; i < stat.n; i++){
-                        if(abs(stat.q[0]-stat.featureVector[i][0])<stat.b_x && abs(stat.q[1]-stat.featureVector[i][1])<stat.b_y && abs(stat.q[2]-stat.featureVector[i][2])<stat.b_z)
-                            density = density + compute_density(stat.q[0],stat.featureVector[i][0],stat.k_type_x,stat.b_x)*compute_density(stat.q[1],stat.featureVector[i][1],stat.k_type_y,stat.b_y)*compute_density(stat.q[2],stat.featureVector[i][2],stat.k_type_z,stat.b_z);
-                    }
-                    stat.out_tensor_3[x_index][y_index][z_index] = density;
-                }
-                
-            }     
-		}
+
     }
+
 }
